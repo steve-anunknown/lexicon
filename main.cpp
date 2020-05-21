d5da157eeefe33b5bc272a3b1c601e842dc7c6d4
steve-anunknown <steve.anunknown@gmail.com>
2020-05-21 19:54:02 +0000

    this actually works

--- main.cpp
+++ main.cpp
@@ -1,204 +1,199 @@
-#include <iostream>
-#include <string>
-
-class lexicon{
-  private:
-
-  struct node {
-    std::string word;
-    
-    unsigned int frequency;
-    
-    node *left,*right,*parent;
-    
-    node (const std::string &w,unsigned int f, node *l, node *r,node *p):
-      word(w),frequency(f),left(l),right(r),parent(p){};
+#include <iostream>  
+#include <string>  
   
-  };
-
-  node *root;
+using namespace std;  
   
-  public:
-
-  lexicon() : root ( nullptr ) {std::cout<<"constructor check\n";};
-  ~lexicon() { purge(root) ; }
-
-  void insert(const std::string &s) {
-    if (root==nullptr) root = new node (s, 1, nullptr, nullptr, nullptr);
-    else insert(root, s);
-    std::cout<<"done\n";
-  }
+class lexicon {  
+    struct node {  
+            string word;  
+            unsigned int freq;  
+            node *left,*right;  
+            node (const string &s,unsigned int f):word(s),freq(f),left(NULL),right(NULL){};  
+    };  
   
-  int lookup(const std::string &s)const { return rec_lookup(root, s); }
+    node *root;  
+    static void purge (node *t){  
+            if (t==NULL) return ;  
+            purge (t->left);  
+            purge(t->right);  
+            delete t;  
+            t=NULL;  
+    }  
+    static void print (ostream &out, node *t){  
+            if (t==NULL) return;  
+            print(out,t->left);  
+            out<<t->word<<' '<<t->freq<<endl;  
+            print(out,t->right);  
+            return ;  
+    }  
   
-  int depth(const std::string &s)const {
-    bool found=false;
-    return rec_depth(root, s, found);
-  }
+    public:  
+    lexicon():root(NULL){}  
+    ~lexicon(){ purge (root) ; root=NULL; }  
   
-  void replace(const std::string &s1, const std::string &s2){
-    node *node_s1=rec_find(root,s1);
-    node *node_s2=rec_find(root,s2);
-    if (node_s1==root)
-    if (node_s1==nullptr) return;
-    if (node_s1!=nullptr && node_s2==nullptr){
-      //remove s1 and insert s2 with s1's frequency
-      int freq=node_s1->frequency-1;
-      remove (node_s1);
-      insert (root, s2);
-      rec_find(root,s2)->frequency+=freq;
-    }
-    if (node_s1!=nullptr && node_s2!=nullptr){
-      //remove s1 and update s2's frequency to s1.freq+s2.freq
-      node_s2->frequency+=node_s1->frequency;
-      remove (node_s1);
-    }
-  }
-
-  friend std::ostream & operator << (std::ostream &out , const lexicon &l){
-    print(out, l.root);
-    return out;
-  }
-
-  private:
-
-
-  static void print (std::ostream &out, node *t){
-    if (t!=nullptr){
-      print(out,t->left);
-      out<<t->word<<' '<<t->frequency<<std::endl;
-      print(out,t->right);
-    }
-  }
-
-
-  static void remove(node *t){
-    //if t has 2 children check
-      //replace with t->left
-    //if t has 1 child check
-      //replace with only child
-    if (t==nullptr) return;
-    else if (t->parent==nullptr){
-      if (t->left!=nullptr && t->right!=nullptr){
-        std::string right_word=t->right->word;
-        t=t->left;
-        insert(t, right_word);
-      }
-      else if (t->left!=nullptr && t->right==nullptr) t=t->left;
-      else if (t->left==nullptr && t->right!=nullptr) t=t->right;
-    }
-    else if (t->parent!=nullptr){
-      if (!is_left_child(t)) {
-        if (t->left!=nullptr && t->right!=nullptr){
-          std::string right_word=t->right->word;
-          t->parent->right=t->left;
-          insert(t->parent, right_word);
-        }
-        else if (t->left!=nullptr && t->right==nullptr) t->parent->right=t->left;
-        else if (t->left==nullptr && t->right!=nullptr) t->parent->right=t->right;
-        delete t;
-      }
-      else {
-        if (t->left!=nullptr && t->right!=nullptr){
-          std::string right_word=t->right->word;
-          t->parent->left=t->left;
-          insert(t->parent, right_word);
-        }
-        else if (t->left!=nullptr && t->right==nullptr) t->parent->left=t->left;
-        else if (t->left==nullptr && t->right!=nullptr) t->parent->left=t->right;
-        delete t;
-      }
-    }
-  }
+    void insert (const string &s){  
+            if (root==NULL) { root=new(nothrow) node (s,1); if (root==NULL) exit(2); return ; }  
+            node *parent=root;  
+            node *runner=root;  
+            while (runner!=NULL){  
+                parent=runner;  
+                if (s>runner->word) runner=runner->right;  
+                else if (s<runner->word) runner=runner->left;  
+                else {++(runner->freq); return ;}  
+            }  
+            if (s>parent->word){  
+                parent->right=new(nothrow) node (s,1);   
+                if(parent->right==NULL) exit(2);  
+                }  
+                else { 
+                  parent->left=new(nothrow) node (s,1);  
+                  if(parent->left==NULL) exit(2);  
+                }  
+    }  
   
-  static bool is_left_child(node *t){
-    if (t==nullptr) return false;
-    if (t->parent->left==t) return true;
-    else return false;
-    }
-
-  node * rec_find(node *t,const std::string &s){
-    if (t!=nullptr){
-      if (s==t->word) return t;
-      else if (s>t->word) return rec_find(t->right, s);
-      else return rec_find(t->left, s); 
-    }
-    else return nullptr;
-  }
+    int lookup(const string &s)const {  
+            node *runner=root;  
+            while (runner!=NULL){  
+                    if (s>runner->word) runner=runner->right;  
+                        else if (s<runner->word) runner=runner->left;  
+                        else  return runner->freq;  
+                }  
+                return 0;  
+        }  
   
-  int rec_depth(node *t,const std::string s, bool &flag)const {
-    if (t==nullptr) {flag=false; return 0;}
-    else if (s==t->word) {flag=true; return 1;} 
-    else if (s>t->word) {
-       int result=rec_depth(t->right, s, flag);
-       (flag==true)? ++result : result=0;
-       return result;
-       }
-    else  {
-       int result=rec_depth (t->left, s, flag);
-       (flag==true)? ++result : result=0;
-       return result;
-       }
-  }
+        int depth (const string &s)const {  
+                node *runner=root;  
+                int result=1;  
+                while (runner!=NULL){  
+                        if (s>runner->word){ runner=runner->right; ++result;}  
+                        else if (s<runner->word){ runner=runner->left; ++result;}  
+                        else  return result;  
+                }  
+                return 0;  
+        }  
   
-  int rec_lookup(node *t, const std::string &s)const{
-    if (t==nullptr) return 0;
-    else if (s>t->word) return rec_lookup(t->right, s);
-    else if (s<t->word) return rec_lookup(t->left, s);
-    else  return t->frequency;
-  }
+        void replace (const string &s1, const string &s2){  
+                if (s1==s2) return ;  
+                node* parent_s1=root;//parent of runner  
+                node* runner_s1=root;//runner points to s1  
+                while (runner_s1!=NULL){  
+                        if (s1>runner_s1->word){parent_s1=runner_s1; runner_s1=runner_s1->right;}  
+                        else if (s1<runner_s1->word){parent_s1=runner_s1; runner_s1=runner_s1->left;}  
+                        else  break;  
+                }  
+                node *parent_s2=root;//parent of runner  
+                node *runner_s2=root;//runner points to s2  
+                while (runner_s2!=NULL){  
+                        if (s2>runner_s2->word){parent_s2=runner_s2; runner_s2=runner_s2->right;}  
+                        else if (s2<runner_s2->word){parent_s2=runner_s2; runner_s2=runner_s2->left;}  
+                        else  break;  
+                }  
+                if (runner_s1==NULL) return ;  
+                unsigned int freq1=runner_s1->freq;  
+                if (runner_s1==root){  
+                        if (runner_s1->left!=NULL){  
+                                root=runner_s1->left;  
+                                node *temppar=runner_s1->left;  
+                                node *temprun=runner_s1->left;  
+                                while (temprun!=NULL){  
+                                        temppar=temprun;  
+                                        temprun=temprun->right;  
+                                }  
+                                temppar->right=runner_s1->right;  
+                        }  
+                        else root=runner_s1->right;  
+                }  
+                else if (parent_s1->left==runner_s1){ //runner is left child  
+                        if (runner_s1->left!=NULL) {  
+                                parent_s1->left=runner_s1->left;  
+                                node *temppar=runner_s1->left;  
+                                node *temprun=runner_s1->left;  
+                                while (temprun!=NULL){  
+                                        temppar=temprun;  
+                                        temprun=temprun->right;  
+                                }  
+                                temppar->right=runner_s1->right;  
+                        }  
+                        else parent_s1->left=runner_s1->right;  
+                }  
+                else { // runner is right child  
+                        if (runner_s1->left!=NULL){  
+                                parent_s1->right=runner_s1->left;  
+                                node *temppar=runner_s1->left;  
+                                node *temprun=runner_s1->left;  
+                                while (temprun!=NULL){  
+                                        temppar=temprun;  
+                                        temprun=temprun->right;  
+                                }  
+                                temppar->right=runner_s1->right;  
+                        }  
+                        else parent_s1->right=runner_s1->right;  
+                }  
+                delete runner_s1 ; runner_s1=NULL;  
+                if (runner_s2==NULL) {  
+                        if (root==NULL) {root= new node (s2,freq1);/* if (root==NULL) exit(3);*/}  
+                        node *temppar=root;  
+                        node *temprun=root;  
+                        while (temprun!=NULL){  
+                                temppar=temprun;  
+                                if (s2>temprun->word) temprun=temprun->right;  
+                                else temprun=temprun->left;  
+                        }  
+                        if (s2>temppar->word){  
+                                temppar->right= new node (s2,freq1);  
+                               // if (temppar->right==NULL) exit(3);  
+                        }  
+                        else {  
+                                temppar->left= new node (s2,freq1);  
+                               // if (temppar->right==NULL) exit(3);  
+                        }  
+                }  
+                else  runner_s2->freq+=freq1;  
+        }  
   
-  static bool insert(node *t, const std::string &s) {
-    if (s < t->word) {
-      if (t->left == nullptr) {
-      t->left = new node (s, 1, nullptr, nullptr, t);
-      return true;
-      }
-      else
-        return insert(t->left, s);
-    }
-    else if (s > t->word) {
-      if (t->right == nullptr) {
-      t->right = new node(s, 1, nullptr, nullptr, t);
-      return true;
-      }
-      else
-        return insert(t->right, s);
-    }
-    else {
-      ++(t->frequency);
-      return true;
-    }
-  }
+        friend ostream & operator << (ostream &out, const lexicon &l){  
+                print(out, l.root);  
+                return out;  
+        }  
+};  
+#ifndef CONTEST  
+int main (){  
+        lexicon l;  
   
-  static void purge(node *t){
-    if (t!=nullptr){
-      purge(t->left);
-      purge(t->right);
-      delete t;
-    }
-  }
-};
-
-#ifndef CONTEST
-int main (){
-  lexicon l;
-  l.insert("the");
-  l.insert("boy");
-  l.insert("and");
-  l.insert("the");
-  l.insert("wolf");
-
-  std::cout<<l.lookup("the")<<"\n";
-  std::cout<<l.depth("and")<<"\n";
-
-  std::cout<<l;
-
-  l.replace("boy", "wolf");
-  std::cout<<l;
-
-  std::cout<<l.depth("and");
-
-  return 0;
-}
-#endif
+        l.insert("the");  
+        l.insert("boy");  
+        l.insert("and");  
+        l.insert("the");  
+        l.insert("wolf");  
+  
+        cout<<l<<endl;  
+          
+        cout<<l.lookup("and")<<' '<<l.depth("and")<<"\n";  
+        cout<<l.lookup("boy")<<' '<<l.depth("boy")<<"\n";  
+        cout<<l.lookup("the")<<' '<<l.depth("the")<<"\n";  
+        cout<<l.lookup("wolf")<<' '<<l.depth("wolf")<<"\n";  
+        cout<<l.lookup("dummy")<<' '<<l.depth("dummy")<<"\n";  
+  
+        l.replace("and","dummy");  
+        cout<<l.lookup("wolf")<<' '<<l.depth("wolf")<<"\n";  
+        cout<<l.lookup("dummy")<<' '<<l.depth("dummy")<<"\n";  
+        cout<<l<<"\n";  
+  
+        l.replace("boy","dummy");  
+        cout<<l.lookup("wolf")<<' '<<l.depth("wolf")<<"\n";  
+        cout<<l.lookup("dummy")<<' '<<l.depth("dummy")<<"\n";  
+        cout<<l<<"\n";  
+  
+        l.replace("the","dummy");  
+        cout<<l.lookup("wolf")<<' '<<l.depth("wolf")<<"\n";  
+        cout<<l.lookup("dummy")<<' '<<l.depth("dummy")<<"\n";  
+        cout<<l<<"\n";  
+        l.replace("wolf","dummy");  
+        cout<<l.lookup("wolf")<<' '<<l.depth("wolf")<<"\n";  
+        cout<<l.lookup("dummy")<<' '<<l.depth("dummy")<<"\n";  
+  
+        cout<<l<<endl;  
+  
+        return 0;  
+}  
+#endif  
